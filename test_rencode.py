c4bed93dcf1092a714255f5e43792ba6d1167f6b
Andrew Resch <andrewresch@gmail.com>
2010-05-23 00:27:28 +0000

    Add support for decoding

--- test_rencode.py
+++ test_rencode.py
@@ -27,66 +27,145 @@ import rencode
 import rencode_orig
 
 class TestRencode(unittest.TestCase):
-    def test_fixed_pos_int(self):
+    def test_encode_fixed_pos_int(self):
         self.assertEqual(rencode.dumps(1), rencode_orig.dumps(1))
         self.assertEqual(rencode.dumps(40), rencode_orig.dumps(40))
 
-    def test_fixed_neg_int(self):
+    def test_encode_fixed_neg_int(self):
         self.assertEqual(rencode.dumps(-10), rencode_orig.dumps(-10))
         self.assertEqual(rencode.dumps(-29), rencode_orig.dumps(-29))
 
-    def test_int_char_size(self):
+    def test_encode_int_char_size(self):
         self.assertEqual(rencode.dumps(100), rencode_orig.dumps(100))
         self.assertEqual(rencode.dumps(-100), rencode_orig.dumps(-100))
 
-    def test_int_short_size(self):
+    def test_encode_int_short_size(self):
         self.assertEqual(rencode.dumps(27123), rencode_orig.dumps(27123))
+        self.assertEqual(rencode.dumps(-27123), rencode_orig.dumps(-27123))
 
-    def test_int_int_size(self):
+    def test_encode_int_int_size(self):
         self.assertEqual(rencode.dumps(7483648), rencode_orig.dumps(7483648))
         self.assertEqual(rencode.dumps(-7483648), rencode_orig.dumps(-7483648))
 
-    def test_int_long_long_size(self):
+    def test_encode_int_long_long_size(self):
         self.assertEqual(rencode.dumps(8223372036854775808), rencode_orig.dumps(8223372036854775808))
         self.assertEqual(rencode.dumps(-8223372036854775808), rencode_orig.dumps(-8223372036854775808))
 
-    def test_int_big_number(self):
+    def test_encode_int_big_number(self):
         n = int("9"*62)
         self.assertEqual(rencode.dumps(n), rencode_orig.dumps(n))
         self.assertRaises(ValueError, rencode.dumps, int("9"*65))
 
-    def test_float_32bit(self):
+    def test_encode_float_32bit(self):
         self.assertEqual(rencode.dumps(1234.56), rencode_orig.dumps(1234.56))
 
-    def test_float_64bit(self):
+    def test_encode_float_64bit(self):
         self.assertEqual(rencode.dumps(1234.56, 64), rencode_orig.dumps(1234.56, 64))
 
-    def test_float_invalid_size(self):
+    def test_encode_float_invalid_size(self):
         self.assertRaises(ValueError, rencode.dumps, 1234.56, 36)
 
-    def test_fixed_str(self):
+    def test_encode_fixed_str(self):
         self.assertEqual(rencode.dumps("foobarbaz"), rencode_orig.dumps("foobarbaz"))
 
-    def test_str(self):
+    def test_encode_str(self):
         self.assertEqual(rencode.dumps("f"*255), rencode_orig.dumps("f"*255))
 
-    def test_unicode(self):
+    def test_encode_unicode(self):
         self.assertEqual(rencode.dumps(u"foobar"), rencode_orig.dumps(u"foobar"))
 
-    def test_none(self):
+    def test_encode_none(self):
         self.assertEqual(rencode.dumps(None), rencode_orig.dumps(None))
 
-    def test_fixed_list(self):
+    def test_encode_bool(self):
+        self.assertEqual(rencode.dumps(True), rencode_orig.dumps(True))
+        self.assertEqual(rencode.dumps(False), rencode_orig.dumps(False))
+
+    def test_encode_fixed_list(self):
         l = [100, -234.01, "foobar", u"baz"]*4
         self.assertEqual(rencode.dumps(l), rencode_orig.dumps(l))
 
-    def test_list(self):
+    def test_encode_list(self):
         l = [100, -234.01, "foobar", u"baz"]*80
         self.assertEqual(rencode.dumps(l), rencode_orig.dumps(l))
 
-    def test_fixed_dict(self):
+    def test_encode_fixed_dict(self):
         s = "abcdefghijk"
         d = dict(zip(s, [1234]*len(s)))
         self.assertEqual(rencode.dumps(d), rencode_orig.dumps(d))
+
+    def test_encode_dict(self):
+        s = "abcdefghijklmnopqrstuvwxyz1234567890"
+        d = dict(zip(s, [1234]*len(s)))
+        self.assertEqual(rencode.dumps(d), rencode_orig.dumps(d))
+
+    def test_decode_fixed_pos_int(self):
+        self.assertEqual(rencode.loads(rencode.dumps(10)), 10)
+
+    def test_decode_fixed_neg_int(self):
+        self.assertEqual(rencode.loads(rencode.dumps(-10)), -10)
+
+    def test_decode_char(self):
+        self.assertEqual(rencode.loads(rencode.dumps(100)), 100)
+        self.assertEqual(rencode.loads(rencode.dumps(-100)), -100)
+
+    def test_decode_short(self):
+        self.assertEqual(rencode.loads(rencode.dumps(27123)), 27123)
+        self.assertEqual(rencode.loads(rencode.dumps(-27123)), -27123)
+
+    def test_decode_int(self):
+        self.assertEqual(rencode.loads(rencode.dumps(7483648)), 7483648)
+        self.assertEqual(rencode.loads(rencode.dumps(-7483648)), -7483648)
+
+    def test_decode_long_long(self):
+        self.assertEqual(rencode.loads(rencode.dumps(8223372036854775808)), 8223372036854775808)
+        self.assertEqual(rencode.loads(rencode.dumps(-8223372036854775808)), -8223372036854775808)
+
+    def test_decode_int_big_number(self):
+        n = int("9"*62)
+        self.assertEqual(rencode.loads(rencode.dumps(n)), n)
+
+    def test_decode_float_32bit(self):
+        f = rencode.dumps(1234.56)
+        self.assertEqual(rencode.loads(f), rencode_orig.loads(f))
+
+    def test_decode_float_64bit(self):
+        f = rencode.dumps(1234.56, 64)
+        self.assertEqual(rencode.loads(f), rencode_orig.loads(f))
+
+    def test_decode_fixed_str(self):
+        self.assertEqual(rencode.loads(rencode.dumps("foobarbaz")), "foobarbaz")
+
+    def test_decode_str(self):
+        self.assertEqual(rencode.loads(rencode.dumps("f"*255)), "f"*255)
+
+    def test_decode_unicode(self):
+        self.assertEqual(rencode.loads(rencode.dumps(u"foobar")), u"foobar")
+
+    def test_decode_none(self):
+        self.assertEqual(rencode.loads(rencode.dumps(None)), None)
+
+    def test_decode_bool(self):
+        self.assertEqual(rencode.loads(rencode.dumps(True)), True)
+        self.assertEqual(rencode.loads(rencode.dumps(False)), False)
+
+    def test_decode_fixed_list(self):
+        l = [100, False, "foobar", u"baz"]*4
+        self.assertEqual(rencode.loads(rencode.dumps(l)), l)
+
+    def test_decode_list(self):
+        l = [100, False, "foobar", u"baz"]*80
+        self.assertEqual(rencode.loads(rencode.dumps(l)), l)
+
+    def test_decode_fixed_dict(self):
+        s = "abcdefghijk"
+        d = dict(zip(s, [1234]*len(s)))
+        self.assertEqual(rencode.loads(rencode.dumps(d)), d)
+
+    def test_decode_dict(self):
+        s = "abcdefghijklmnopqrstuvwxyz1234567890"
+        d = dict(zip(s, [1234]*len(s)))
+        self.assertEqual(rencode.loads(rencode.dumps(d)), d)
+
 if __name__ == '__main__':
     unittest.main()
