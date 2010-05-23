6869127d4c5688c9a505e272f760452e425a114d
andrewresch <andrewresch@c7ebcaa3-740d-fc88-45b6-64c082fd2f34>
2010-05-23 01:19:45 +0000

    Add decoding functions time tests

--- trunk/timetest.py
+++ trunk/timetest.py
@@ -1,6 +1,31 @@
+#
+# timetest.py
+#
+# Copyright (C) 2010 Andrew Resch <andrewresch@gmail.com>
+#
+# Deluge is free software.
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# deluge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with deluge.    If not, write to:
+#     The Free Software Foundation, Inc.,
+#     51 Franklin Street, Fifth Floor
+#     Boston, MA  02110-1301, USA.
+#
+
 import rencode
 import rencode_orig
 
+# Encode functions
 
 def test_encode_fixed_pos_int():
     rencode.dumps(40)
@@ -78,12 +103,6 @@ def test_encode_str():
 def test_encode_str_orig():
     rencode_orig.dumps(s)
 
-def test_encode_unicode():
-    rencode.dumps(u"foobar")
-
-def test_encode_unicode_orig():
-    rencode_orig.dumps(u"foobar")
-
 def test_encode_none():
     rencode.dumps(None)
 
@@ -120,13 +139,127 @@ def test_encode_fixed_dict_orig():
     rencode_orig.dumps(d)
 
 keys2 = "abcdefghijklmnopqrstuvwxyz1234567890"
-d2 = dict(zip(keys, [None]*len(keys)))
+d2 = dict(zip(keys2, [None]*len(keys2)))
 
 def test_encode_dict():
     rencode.dumps(d2)
 
 def test_encode_dict_orig():
     rencode_orig.dumps(d2)
+
+
+# Decode functions
+
+def test_decode_fixed_pos_int():
+    rencode.loads('(')
+
+def test_decode_fixed_pos_int_orig():
+    rencode_orig.loads('(')
+
+def test_decode_fixed_neg_int():
+    rencode.loads('b')
+
+def test_decode_fixed_neg_int_orig():
+    rencode_orig.loads('b')
+
+def test_decode_int_char_size():
+    rencode.loads('>d')
+    rencode.loads('>\x9c')
+
+def test_decode_int_char_size_orig():
+    rencode_orig.loads('>d')
+    rencode_orig.loads('>\x9c')
+
+def test_decode_int_short_size():
+    rencode.loads('?i\xf3')
+    rencode.loads('?\x96\r')
+
+def test_decode_int_short_size_orig():
+    rencode_orig.loads('?i\xf3')
+    rencode_orig.loads('?\x96\r')
+
+def test_decode_int_int_size():
+    rencode.loads('@\x00r1\x00')
+    rencode.loads('@\xff\x8d\xcf\x00')
+
+def test_decode_int_int_size_orig():
+    rencode_orig.loads('@\x00r1\x00')
+    rencode_orig.loads('@\xff\x8d\xcf\x00')
+
+def test_decode_int_long_long_size():
+    rencode.loads('Ar\x1fILX\x9c\x00\x00')
+    rencode.loads('A\x8d\xe0\xb6\xb3\xa7d\x00\x00')
+
+def test_decode_int_long_long_size_orig():
+    rencode_orig.loads('Ar\x1fILX\x9c\x00\x00')
+    rencode_orig.loads('A\x8d\xe0\xb6\xb3\xa7d\x00\x00')
+
+def test_decode_int_big_number():
+    rencode.loads('=99999999999999999999999999999999999999999999999999999999999999\x7f')
+
+def test_decode_int_big_number_orig():
+    rencode_orig.loads('=99999999999999999999999999999999999999999999999999999999999999\x7f')
+
+def test_decode_float_32bit():
+    rencode.loads('BD\x9aQ\xec')
+
+def test_decode_float_32bit_orig():
+    rencode_orig.loads('BD\x9aQ\xec')
+
+def test_decode_float_64bit():
+    rencode.loads(',@\x93J=p\xa3\xd7\n')
+
+def test_decode_float_64bit_orig():
+    rencode_orig.loads(',@\x93J=p\xa3\xd7\n')
+
+def test_decode_fixed_str():
+    rencode.loads('\x89foobarbaz')
+
+def test_decode_fixed_str_orig():
+    rencode_orig.loads('\x89foobarbaz')
+
+s = "f"*255
+def test_decode_str():
+    rencode.loads('255:fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+
+def test_decode_str_orig():
+    rencode_orig.loads('255:fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+
+def test_decode_none():
+    rencode.loads('E')
+
+def test_decode_none_orig():
+    rencode_orig.loads('E')
+
+def test_decode_bool():
+    rencode.loads('C')
+
+def test_decode_bool_orig():
+    rencode_orig.loads('C')
+
+def test_decode_fixed_list():
+    rencode.loads('\xc4EEEE')
+
+def test_decode_fixed_list_orig():
+    rencode_orig.loads('\xc4EEEE')
+
+def test_decode_list():
+    rencode.loads(';EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\x7f')
+
+def test_decode_list_orig():
+    rencode_orig.loads(';EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\x7f')
+
+def test_decode_fixed_dict():
+    rencode.loads('q\x81aE\x81cE\x81bE\x81eE\x81dE\x81gE\x81fE\x81iE\x81hE\x81kE\x81jE')
+
+def test_decode_fixed_dict_orig():
+    rencode_orig.loads('q\x81aE\x81cE\x81bE\x81eE\x81dE\x81gE\x81fE\x81iE\x81hE\x81kE\x81jE')
+
+def test_decode_dict():
+    rencode.loads('<\x811E\x810E\x813E\x812E\x815E\x814E\x817E\x816E\x819E\x818E\x81aE\x81cE\x81bE\x81eE\x81dE\x81gE\x81fE\x81iE\x81hE\x81kE\x81jE\x81mE\x81lE\x81oE\x81nE\x81qE\x81pE\x81sE\x81rE\x81uE\x81tE\x81wE\x81vE\x81yE\x81xE\x81zE\x7f')
+
+def test_decode_dict_orig():
+    rencode_orig.loads('<\x811E\x810E\x813E\x812E\x815E\x814E\x817E\x816E\x819E\x818E\x81aE\x81cE\x81bE\x81eE\x81dE\x81gE\x81fE\x81iE\x81hE\x81kE\x81jE\x81mE\x81lE\x81oE\x81nE\x81qE\x81pE\x81sE\x81rE\x81uE\x81tE\x81wE\x81vE\x81yE\x81xE\x81zE\x7f')
 
 
 overall = [
@@ -165,8 +298,8 @@ if __name__ == "__main__":
         new_time = timeit.Timer("%s()" % func, "from __main__ import %s" % func).timeit(iterations)
         orig_time = timeit.Timer("%s_orig()" % func, "from __main__ import %s_orig" % func).timeit(iterations)
         if new_time > orig_time:
-            new = CSI + "31m%.3fs%s (+%.3fs)" % (new_time, reset, new_time-orig_time)
-            orig = CSI + "32m%.3fs%s" % (orig_time, reset)
+            new = CSI + "31m%.3fs%s" % (new_time, reset)
+            orig = CSI + "32m%.3fs%s (%s34m+%.3fs%s) %.2f%%" % (orig_time, reset, CSI, new_time-orig_time, reset, (new_time/orig_time)*100)
         else:
             new = CSI + "32m%.3fs%s (%s34m+%.3fs%s) %.2f%%" % (new_time, reset, CSI, orig_time-new_time, reset, (orig_time/new_time)*100)
             orig = CSI + "31m%.3fs%s" % (orig_time, reset)
@@ -178,24 +311,28 @@ if __name__ == "__main__":
 
     loc = locals().keys()
 
-    print "Encoding functions:"
-    print "*" * 79
-    print ""
+    for t in ("encode", "decode"):
+        print "*" * 79
+        print "%sing functions:" % (t[:-1].title())
+        print "*" * 79
+        print ""
 
-    total_new = 0.0
-    total_orig = 0.0
-    for func in loc:
-        if func.startswith("test_encode_") and not func.endswith("_orig"):
-            n, o = do_test(func)
-            total_new += n
-            total_orig += o
+        total_new = 0.0
+        total_orig = 0.0
+        for func in loc:
+            if func.startswith("test_%s_" % t) and not func.endswith("_orig"):
+                n, o = do_test(func)
+                total_new += n
+                total_orig += o
 
-    print "Encoding functions totals:"
-    if total_new > total_orig:
-        pass
-    else:
-        new = "%s32m%.3fs%s (%s34m+%.3fs%s) %.2f%%" % (CSI, total_new, reset, CSI, total_orig-total_new, reset, (total_orig/total_new)*100)
-        orig = CSI + "31m%.3fs%s" % (total_orig, reset)
+        print "%sing functions totals:" % (t[:-1].title())
+        if total_new > total_orig:
+            new = CSI + "31m%.3fs%s" % (total_new, reset)
+            orig = "%s32m%.3fs%s (%s34m+%.3fs%s) %.2f%%" % (CSI, total_orig, reset, CSI, total_new-total_orig, reset, (total_new/total_orig)*100)
+        else:
+            new = "%s32m%.3fs%s (%s34m+%.3fs%s) %.2f%%" % (CSI, total_new, reset, CSI, total_orig-total_new, reset, (total_orig/total_new)*100)
+            orig = CSI + "31m%.3fs%s" % (total_orig, reset)
 
-    print "\trencode.pyx: %s" % new
-    print "\trencode.py:  %s" % orig
+        print "\trencode.pyx: %s" % new
+        print "\trencode.py:  %s" % orig
+        print ""
