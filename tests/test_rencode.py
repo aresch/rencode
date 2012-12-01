1e5ecd93ebde80af81fb607f3898ab842cd65b7b
Andrew Resch <andrewresch@gmail.com>
2012-12-01 22:56:15 +0000

    Move rencode_orig.py to tests/

--- tests/test_rencode.py
+++ tests/test_rencode.py
@@ -25,7 +25,7 @@
 
 import unittest
 from rencode import _rencode as rencode
-from rencode import rencode_orig
+import rencode_orig
 
 class TestRencode(unittest.TestCase):
     def test_encode_fixed_pos_int(self):
