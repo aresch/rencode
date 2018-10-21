8c4cde72b57c0e4b34b9de57b625457cb22b1ea2
Andrew Resch <andrewresch@gmail.com>
2018-10-21 11:26:29 -0700

    Remove testing from the build-wheels.sh script

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -15,8 +15,7 @@ for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
-# Install packages and test
+# Install packages
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install rencode --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/pytest" rencode)
 done
