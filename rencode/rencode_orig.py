ccb2b8b4fcb2066c9f1a2386989c67cbbfc893a8
Andrew Resch <andrewresch@gmail.com>
2021-08-09 19:56:56 -0700

    Format code with Black

--- rencode/rencode_orig.py
+++ rencode/rencode_orig.py
@@ -64,7 +64,7 @@ import sys
 from threading import Lock
 
 __version__ = ("Python", 1, 0, 6)
-__all__ = ('dumps', 'loads')
+__all__ = ("dumps", "loads")
 
 py3 = sys.version_info[0] >= 3
 if py3:
@@ -73,9 +73,13 @@ if py3:
 
     def int2byte(c):
         return bytes([c])
+
+
 else:
+
     def int2byte(c):
         return chr(c)
+
 
 # Default number of bits for serialized floats, either 32 or 64 (also a parameter for dumps()).
 DEFAULT_FLOAT_BITS = 32
@@ -127,80 +131,80 @@ def decode_int(x, f):
     f += 1
     newf = x.index(CHR_TERM, f)
     if newf - f >= MAX_INT_LENGTH:
-        raise ValueError('overflow')
+        raise ValueError("overflow")
     try:
         n = int(x[f:newf])
     except (OverflowError, ValueError):
         n = long(x[f:newf])
-    if x[f:f + 1] == '-':
-        if x[f + 1:f + 2] == '0':
+    if x[f : f + 1] == "-":
+        if x[f + 1 : f + 2] == "0":
             raise ValueError
-    elif x[f:f + 1] == '0' and newf != f + 1:
+    elif x[f : f + 1] == "0" and newf != f + 1:
         raise ValueError
     return (n, newf + 1)
 
 
 def decode_intb(x, f):
     f += 1
-    return (struct.unpack('!b', x[f:f + 1])[0], f + 1)
+    return (struct.unpack("!b", x[f : f + 1])[0], f + 1)
 
 
 def decode_inth(x, f):
     f += 1
-    return (struct.unpack('!h', x[f:f + 2])[0], f + 2)
+    return (struct.unpack("!h", x[f : f + 2])[0], f + 2)
 
 
 def decode_intl(x, f):
     f += 1
 
-    return (struct.unpack('!l', x[f:f + 4])[0], f + 4)
+    return (struct.unpack("!l", x[f : f + 4])[0], f + 4)
 
 
 def decode_intq(x, f):
     f += 1
-    return (struct.unpack('!q', x[f:f + 8])[0], f + 8)
+    return (struct.unpack("!q", x[f : f + 8])[0], f + 8)
 
 
 def decode_float32(x, f):
     f += 1
-    n = struct.unpack('!f', x[f:f + 4])[0]
+    n = struct.unpack("!f", x[f : f + 4])[0]
     return (n, f + 4)
 
 
 def decode_float64(x, f):
     f += 1
-    n = struct.unpack('!d', x[f:f + 8])[0]
+    n = struct.unpack("!d", x[f : f + 8])[0]
     return (n, f + 8)
 
 
 def decode_string(x, f):
-    colon = x.index(b':', f)
+    colon = x.index(b":", f)
     try:
         n = int(x[f:colon])
     except (OverflowError, ValueError):
         n = long(x[f:colon])
-    if x[f] == '0' and colon != f + 1:
+    if x[f] == "0" and colon != f + 1:
         raise ValueError
     colon += 1
-    s = x[colon:colon + n]
+    s = x[colon : colon + n]
     if _decode_utf8:
-        s = s.decode('utf8')
+        s = s.decode("utf8")
     return (s, colon + n)
 
 
 def decode_list(x, f):
     r, f = [], f + 1
-    while x[f:f + 1] != CHR_TERM:
-        v, f = decode_func[x[f:f + 1]](x, f)
+    while x[f : f + 1] != CHR_TERM:
+        v, f = decode_func[x[f : f + 1]](x, f)
         r.append(v)
     return (tuple(r), f + 1)
 
 
 def decode_dict(x, f):
     r, f = {}, f + 1
-    while x[f:f + 1] != CHR_TERM:
-        k, f = decode_func[x[f:f + 1]](x, f)
-        r[k], f = decode_func[x[f:f + 1]](x, f)
+    while x[f : f + 1] != CHR_TERM:
+        k, f = decode_func[x[f : f + 1]](x, f)
+        r[k], f = decode_func[x[f : f + 1]](x, f)
     return (r, f + 1)
 
 
@@ -215,17 +219,18 @@ def decode_false(x, f):
 def decode_none(x, f):
     return (None, f + 1)
 
+
 decode_func = {}
-decode_func[b'0'] = decode_string
-decode_func[b'1'] = decode_string
-decode_func[b'2'] = decode_string
-decode_func[b'3'] = decode_string
-decode_func[b'4'] = decode_string
-decode_func[b'5'] = decode_string
-decode_func[b'6'] = decode_string
-decode_func[b'7'] = decode_string
-decode_func[b'8'] = decode_string
-decode_func[b'9'] = decode_string
+decode_func[b"0"] = decode_string
+decode_func[b"1"] = decode_string
+decode_func[b"2"] = decode_string
+decode_func[b"3"] = decode_string
+decode_func[b"4"] = decode_string
+decode_func[b"5"] = decode_string
+decode_func[b"6"] = decode_string
+decode_func[b"7"] = decode_string
+decode_func[b"8"] = decode_string
+decode_func[b"9"] = decode_string
 decode_func[CHR_LIST] = decode_list
 decode_func[CHR_DICT] = decode_dict
 decode_func[CHR_INT] = decode_int
@@ -243,13 +248,16 @@ decode_func[CHR_NONE] = decode_none
 def make_fixed_length_string_decoders():
     def make_decoder(slen):
         def f(x, f):
-            s = x[f + 1:f + 1 + slen]
+            s = x[f + 1 : f + 1 + slen]
             if _decode_utf8:
                 s = s.decode("utf8")
             return (s, f + 1 + slen)
+
         return f
+
     for i in range(STR_FIXED_COUNT):
         decode_func[int2byte(STR_FIXED_START + i)] = make_decoder(i)
+
 
 make_fixed_length_string_decoders()
 
@@ -259,12 +267,15 @@ def make_fixed_length_list_decoders():
         def f(x, f):
             r, f = [], f + 1
             for _ in range(slen):
-                v, f = decode_func[x[f:f + 1]](x, f)
+                v, f = decode_func[x[f : f + 1]](x, f)
                 r.append(v)
             return (tuple(r), f)
+
         return f
+
     for i in range(LIST_FIXED_COUNT):
         decode_func[int2byte(LIST_FIXED_START + i)] = make_decoder(i)
+
 
 make_fixed_length_list_decoders()
 
@@ -273,11 +284,14 @@ def make_fixed_length_int_decoders():
     def make_decoder(j):
         def f(x, f):
             return (j, f + 1)
+
         return f
+
     for i in range(INT_POS_FIXED_COUNT):
         decode_func[int2byte(INT_POS_FIXED_START + i)] = make_decoder(i)
     for i in range(INT_NEG_FIXED_COUNT):
         decode_func[int2byte(INT_NEG_FIXED_START + i)] = make_decoder(-1 - i)
+
 
 make_fixed_length_int_decoders()
 
@@ -287,12 +301,15 @@ def make_fixed_length_dict_decoders():
         def f(x, f):
             r, f = {}, f + 1
             for _ in range(slen):
-                k, f = decode_func[x[f:f + 1]](x, f)
-                r[k], f = decode_func[x[f:f + 1]](x, f)
+                k, f = decode_func[x[f : f + 1]](x, f)
+                r[k], f = decode_func[x[f : f + 1]](x, f)
             return (r, f)
+
         return f
+
     for i in range(DICT_FIXED_COUNT):
         decode_func[int2byte(DICT_FIXED_START + i)] = make_decoder(i)
+
 
 make_fixed_length_dict_decoders()
 
@@ -315,29 +332,29 @@ def encode_int(x, r):
     elif -INT_NEG_FIXED_COUNT <= x < 0:
         r.append(int2byte(INT_NEG_FIXED_START - 1 - x))
     elif -128 <= x < 128:
-        r.extend((CHR_INT1, struct.pack('!b', x)))
+        r.extend((CHR_INT1, struct.pack("!b", x)))
     elif -32768 <= x < 32768:
-        r.extend((CHR_INT2, struct.pack('!h', x)))
+        r.extend((CHR_INT2, struct.pack("!h", x)))
     elif -2147483648 <= x < 2147483648:
-        r.extend((CHR_INT4, struct.pack('!l', x)))
+        r.extend((CHR_INT4, struct.pack("!l", x)))
     elif -9223372036854775808 <= x < 9223372036854775808:
-        r.extend((CHR_INT8, struct.pack('!q', x)))
+        r.extend((CHR_INT8, struct.pack("!q", x)))
     else:
         s = str(x)
         if py3:
             s = bytes(s, "ascii")
 
         if len(s) >= MAX_INT_LENGTH:
-            raise ValueError('overflow')
+            raise ValueError("overflow")
         r.extend((CHR_INT, s, CHR_TERM))
 
 
 def encode_float32(x, r):
-    r.extend((CHR_FLOAT32, struct.pack('!f', x)))
+    r.extend((CHR_FLOAT32, struct.pack("!f", x)))
 
 
 def encode_float64(x, r):
-    r.extend((CHR_FLOAT64, struct.pack('!d', x)))
+    r.extend((CHR_FLOAT64, struct.pack("!d", x)))
 
 
 def encode_bool(x, r):
@@ -355,7 +372,7 @@ def encode_string(x, r):
         s = str(len(x))
         if py3:
             s = bytes(s, "ascii")
-        r.extend((s, b':', x))
+        r.extend((s, b":", x))
 
 
 def encode_unicode(x, r):
@@ -387,6 +404,7 @@ def encode_dict(x, r):
             encode_func[type(v)](v, r)
         r.append(CHR_TERM)
 
+
 encode_func = {}
 encode_func[int] = encode_int
 encode_func[long] = encode_int
@@ -413,35 +431,67 @@ def dumps(x, float_bits=DEFAULT_FLOAT_BITS):
         elif float_bits == 64:
             encode_func[float] = encode_float64
         else:
-            raise ValueError('Float bits (%d) is not 32 or 64' % float_bits)
+            raise ValueError("Float bits (%d) is not 32 or 64" % float_bits)
         r = []
         encode_func[type(x)](x, r)
-    return b''.join(r)
+    return b"".join(r)
 
 
 def test():
-    f1 = struct.unpack('!f', struct.pack('!f', 25.5))[0]
-    f2 = struct.unpack('!f', struct.pack('!f', 29.3))[0]
-    f3 = struct.unpack('!f', struct.pack('!f', -0.6))[0]
-    ld = (({b'a': 15, b'bb': f1, b'ccc': f2, b'': (f3, (), False, True, b'')}, (b'a', 10**20),
-           tuple(range(-100000, 100000)), b'b' * 31, b'b' * 62, b'b' * 64, 2**30, 2**33, 2**62,
-           2**64, 2**30, 2**33, 2**62, 2**64, False, False, True, -1, 2, 0),)
+    f1 = struct.unpack("!f", struct.pack("!f", 25.5))[0]
+    f2 = struct.unpack("!f", struct.pack("!f", 29.3))[0]
+    f3 = struct.unpack("!f", struct.pack("!f", -0.6))[0]
+    ld = (
+        (
+            {b"a": 15, b"bb": f1, b"ccc": f2, b"": (f3, (), False, True, b"")},
+            (b"a", 10 ** 20),
+            tuple(range(-100000, 100000)),
+            b"b" * 31,
+            b"b" * 62,
+            b"b" * 64,
+            2 ** 30,
+            2 ** 33,
+            2 ** 62,
+            2 ** 64,
+            2 ** 30,
+            2 ** 33,
+            2 ** 62,
+            2 ** 64,
+            False,
+            False,
+            True,
+            -1,
+            2,
+            0,
+        ),
+    )
     assert loads(dumps(ld)) == ld
     d = dict(zip(range(-100000, 100000), range(-100000, 100000)))
-    d.update({b'a': 20, 20: 40, 40: 41, f1: f2, f2: f3, f3: False, False: True, True: False})
-    ld = (d, {}, {5: 6}, {7: 7, True: 8}, {9: 10, 22: 39, 49: 50, 44: b''})
+    d.update(
+        {b"a": 20, 20: 40, 40: 41, f1: f2, f2: f3, f3: False, False: True, True: False}
+    )
+    ld = (d, {}, {5: 6}, {7: 7, True: 8}, {9: 10, 22: 39, 49: 50, 44: b""})
     assert loads(dumps(ld)) == ld
-    ld = (b'', b'a' * 10, b'a' * 100, b'a' * 1000, b'a' * 10000, b'a' * 100000, b'a' * 1000000, b'a' * 10000000)
+    ld = (
+        b"",
+        b"a" * 10,
+        b"a" * 100,
+        b"a" * 1000,
+        b"a" * 10000,
+        b"a" * 100000,
+        b"a" * 1000000,
+        b"a" * 10000000,
+    )
     assert loads(dumps(ld)) == ld
-    ld = tuple([dict(zip(range(n), range(n))) for n in range(100)]) + (b'b',)
+    ld = tuple([dict(zip(range(n), range(n))) for n in range(100)]) + (b"b",)
     assert loads(dumps(ld)) == ld
-    ld = tuple([dict(zip(range(n), range(-n, 0))) for n in range(100)]) + (b'b',)
+    ld = tuple([dict(zip(range(n), range(-n, 0))) for n in range(100)]) + (b"b",)
     assert loads(dumps(ld)) == ld
-    ld = tuple([tuple(range(n)) for n in range(100)]) + (b'b',)
+    ld = tuple([tuple(range(n)) for n in range(100)]) + (b"b",)
     assert loads(dumps(ld)) == ld
-    ld = tuple([b'a' * n for n in range(1000)]) + (b'b',)
+    ld = tuple([b"a" * n for n in range(1000)]) + (b"b",)
     assert loads(dumps(ld)) == ld
-    ld = tuple([b'a' * n for n in range(1000)]) + (None, True, None)
+    ld = tuple([b"a" * n for n in range(1000)]) + (None, True, None)
     assert loads(dumps(ld)) == ld
     assert loads(dumps(None)) is None
     assert loads(dumps({None: None})) == {None: None}
@@ -449,13 +499,16 @@ def test():
     assert 1e-10 < abs(loads(dumps(1.1, 32)) - 1.1) < 1e-6
     assert abs(loads(dumps(1.1, 64)) - 1.1) < 1e-12
     assert loads(dumps("Hello World!!"), decode_utf8=True)
+
+
 try:
     import psyco
+
     psyco.bind(dumps)
     psyco.bind(loads)
 except ImportError:
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
--- tests/test_rencode.py
+++ tests/test_rencode.py
@@ -31,12 +31,16 @@ from rencode import rencode_orig
 
 
 # Hack to deal with python 2 and 3 differences with unicode literals.
-if sys.version < '3':
+if sys.version < "3":
     import codecs
+
     def u(x):
         return codecs.unicode_escape_decode(x)[0]
+
+
 else:
     unicode = str
+
     def u(x):
         return x
 
@@ -63,13 +67,18 @@ class TestRencode(unittest.TestCase):
         self.assertEqual(rencode.dumps(-7483648), rencode_orig.dumps(-7483648))
 
     def test_encode_int_long_long_size(self):
-        self.assertEqual(rencode.dumps(8223372036854775808), rencode_orig.dumps(8223372036854775808))
-        self.assertEqual(rencode.dumps(-8223372036854775808), rencode_orig.dumps(-8223372036854775808))
+        self.assertEqual(
+            rencode.dumps(8223372036854775808), rencode_orig.dumps(8223372036854775808)
+        )
+        self.assertEqual(
+            rencode.dumps(-8223372036854775808),
+            rencode_orig.dumps(-8223372036854775808),
+        )
 
     def test_encode_int_big_number(self):
-        n = int("9"*62)
+        n = int("9" * 62)
         self.assertEqual(rencode.dumps(n), rencode_orig.dumps(n))
-        self.assertRaises(ValueError, rencode.dumps, int("9"*65))
+        self.assertRaises(ValueError, rencode.dumps, int("9" * 65))
 
     def test_encode_float_32bit(self):
         self.assertEqual(rencode.dumps(1234.56), rencode_orig.dumps(1234.56))
@@ -84,7 +93,7 @@ class TestRencode(unittest.TestCase):
         self.assertEqual(rencode.dumps(b"foobarbaz"), rencode_orig.dumps(b"foobarbaz"))
 
     def test_encode_str(self):
-        self.assertEqual(rencode.dumps(b"f"*255), rencode_orig.dumps(b"f"*255))
+        self.assertEqual(rencode.dumps(b"f" * 255), rencode_orig.dumps(b"f" * 255))
         self.assertEqual(rencode.dumps(b"\0"), rencode_orig.dumps(b"\0"))
 
     def test_encode_unicode(self):
@@ -98,21 +107,21 @@ class TestRencode(unittest.TestCase):
         self.assertEqual(rencode.dumps(False), rencode_orig.dumps(False))
 
     def test_encode_fixed_list(self):
-        l = [100, -234.01, b"foobar", u("bäz")]*4
+        l = [100, -234.01, b"foobar", u("bäz")] * 4
         self.assertEqual(rencode.dumps(l), rencode_orig.dumps(l))
 
     def test_encode_list(self):
-        l = [100, -234.01, b"foobar", u("bäz")]*80
+        l = [100, -234.01, b"foobar", u("bäz")] * 80
         self.assertEqual(rencode.dumps(l), rencode_orig.dumps(l))
 
     def test_encode_fixed_dict(self):
         s = b"abcdefghijk"
-        d = dict(zip(s, [1234]*len(s)))
+        d = dict(zip(s, [1234] * len(s)))
         self.assertEqual(rencode.dumps(d), rencode_orig.dumps(d))
 
     def test_encode_dict(self):
         s = b"abcdefghijklmnopqrstuvwxyz1234567890"
-        d = dict(zip(s, [1234]*len(s)))
+        d = dict(zip(s, [1234] * len(s)))
         self.assertEqual(rencode.dumps(d), rencode_orig.dumps(d))
 
     def test_decode_fixed_pos_int(self):
@@ -137,13 +146,17 @@ class TestRencode(unittest.TestCase):
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([64])))
 
     def test_decode_long_long(self):
-        self.assertEqual(rencode.loads(rencode.dumps(8223372036854775808)), 8223372036854775808)
-        self.assertEqual(rencode.loads(rencode.dumps(-8223372036854775808)), -8223372036854775808)
+        self.assertEqual(
+            rencode.loads(rencode.dumps(8223372036854775808)), 8223372036854775808
+        )
+        self.assertEqual(
+            rencode.loads(rencode.dumps(-8223372036854775808)), -8223372036854775808
+        )
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([65])))
 
     def test_decode_int_big_number(self):
-        n = int(b"9"*62)
-        toobig = '={x}\x7f'.format(x='9'*65).encode()
+        n = int(b"9" * 62)
+        toobig = "={x}\x7f".format(x="9" * 65).encode()
         self.assertEqual(rencode.loads(rencode.dumps(n)), n)
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([61])))
         self.assertRaises(ValueError, rencode.loads, toobig)
@@ -163,11 +176,13 @@ class TestRencode(unittest.TestCase):
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([130])))
 
     def test_decode_str(self):
-        self.assertEqual(rencode.loads(rencode.dumps(b"f"*255)), b"f"*255)
+        self.assertEqual(rencode.loads(rencode.dumps(b"f" * 255)), b"f" * 255)
         self.assertRaises(IndexError, rencode.loads, b"50")
 
     def test_decode_unicode(self):
-        self.assertEqual(rencode.loads(rencode.dumps(u("fööbar"))), u("fööbar").encode("utf8"))
+        self.assertEqual(
+            rencode.loads(rencode.dumps(u("fööbar"))), u("fööbar").encode("utf8")
+        )
 
     def test_decode_none(self):
         self.assertEqual(rencode.loads(rencode.dumps(None)), None)
@@ -177,24 +192,24 @@ class TestRencode(unittest.TestCase):
         self.assertEqual(rencode.loads(rencode.dumps(False)), False)
 
     def test_decode_fixed_list(self):
-        l = [100, False, b"foobar", u("bäz").encode("utf8")]*4
+        l = [100, False, b"foobar", u("bäz").encode("utf8")] * 4
         self.assertEqual(rencode.loads(rencode.dumps(l)), tuple(l))
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([194])))
 
     def test_decode_list(self):
-        l = [100, False, b"foobar", u("bäz").encode("utf8")]*80
+        l = [100, False, b"foobar", u("bäz").encode("utf8")] * 80
         self.assertEqual(rencode.loads(rencode.dumps(l)), tuple(l))
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([59])))
 
     def test_decode_fixed_dict(self):
         s = b"abcdefghijk"
-        d = dict(zip(s, [1234]*len(s)))
+        d = dict(zip(s, [1234] * len(s)))
         self.assertEqual(rencode.loads(rencode.dumps(d)), d)
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([104])))
 
     def test_decode_dict(self):
         s = b"abcdefghijklmnopqrstuvwxyz1234567890"
-        d = dict(zip(s, [b"foo"*120]*len(s)))
+        d = dict(zip(s, [b"foo" * 120] * len(s)))
         d2 = {b"foo": d, b"bar": d, b"baz": d}
         self.assertEqual(rencode.loads(rencode.dumps(d2)), d2)
         self.assertRaises(IndexError, rencode.loads, bytes(bytearray([60])))
@@ -205,23 +220,187 @@ class TestRencode(unittest.TestCase):
         self.assertEqual(rencode.loads(rencode.dumps(d)), d)
 
     def test_decode_str_nullbytes(self):
-        b = (202, 132, 100, 114, 97, 119, 1, 0, 0, 63, 1, 242, 63, 1, 60, 132, 120, 50, 54, 52, 49, 51, 48, 58, 0, 0, 0, 1, 65, 154, 35, 215, 48, 204, 4, 35, 242, 3, 122, 218, 67, 192, 127, 40, 241, 127, 2, 86, 240, 63, 135, 177, 23, 119, 63, 31, 226, 248, 19, 13, 192, 111, 74, 126, 2, 15, 240, 31, 239, 48, 85, 238, 159, 155, 197, 241, 23, 119, 63, 2, 23, 245, 63, 24, 240, 86, 36, 176, 15, 187, 185, 248, 242, 255, 0, 126, 123, 141, 206, 60, 188, 1, 27, 254, 141, 169, 132, 93, 220, 252, 121, 184, 8, 31, 224, 63, 244, 226, 75, 224, 119, 135, 229, 248, 3, 243, 248, 220, 227, 203, 193, 3, 224, 127, 47, 134, 59, 5, 99, 249, 254, 35, 196, 127, 17, 252, 71, 136, 254, 35, 196, 112, 4, 177, 3, 63, 5, 220)
+        b = (
+            202,
+            132,
+            100,
+            114,
+            97,
+            119,
+            1,
+            0,
+            0,
+            63,
+            1,
+            242,
+            63,
+            1,
+            60,
+            132,
+            120,
+            50,
+            54,
+            52,
+            49,
+            51,
+            48,
+            58,
+            0,
+            0,
+            0,
+            1,
+            65,
+            154,
+            35,
+            215,
+            48,
+            204,
+            4,
+            35,
+            242,
+            3,
+            122,
+            218,
+            67,
+            192,
+            127,
+            40,
+            241,
+            127,
+            2,
+            86,
+            240,
+            63,
+            135,
+            177,
+            23,
+            119,
+            63,
+            31,
+            226,
+            248,
+            19,
+            13,
+            192,
+            111,
+            74,
+            126,
+            2,
+            15,
+            240,
+            31,
+            239,
+            48,
+            85,
+            238,
+            159,
+            155,
+            197,
+            241,
+            23,
+            119,
+            63,
+            2,
+            23,
+            245,
+            63,
+            24,
+            240,
+            86,
+            36,
+            176,
+            15,
+            187,
+            185,
+            248,
+            242,
+            255,
+            0,
+            126,
+            123,
+            141,
+            206,
+            60,
+            188,
+            1,
+            27,
+            254,
+            141,
+            169,
+            132,
+            93,
+            220,
+            252,
+            121,
+            184,
+            8,
+            31,
+            224,
+            63,
+            244,
+            226,
+            75,
+            224,
+            119,
+            135,
+            229,
+            248,
+            3,
+            243,
+            248,
+            220,
+            227,
+            203,
+            193,
+            3,
+            224,
+            127,
+            47,
+            134,
+            59,
+            5,
+            99,
+            249,
+            254,
+            35,
+            196,
+            127,
+            17,
+            252,
+            71,
+            136,
+            254,
+            35,
+            196,
+            112,
+            4,
+            177,
+            3,
+            63,
+            5,
+            220,
+        )
         d = bytes(bytearray(b))
         self.assertEqual(rencode.loads(rencode.dumps(d)), d)
 
     def test_decode_utf8(self):
         s = b"foobarbaz"
-        #no assertIsInstance with python2.6
+        # no assertIsInstance with python2.6
         d = rencode.loads(rencode.dumps(s), decode_utf8=True)
         if not isinstance(d, unicode):
-            self.fail('%s is not an instance of %r' % (repr(d), unicode))
+            self.fail("%s is not an instance of %r" % (repr(d), unicode))
         s = rencode.dumps(b"\x56\xe4foo\xc3")
         self.assertRaises(UnicodeDecodeError, rencode.loads, s, decode_utf8=True)
 
     def test_version_exposed(self):
         assert rencode.__version__
         assert rencode_orig.__version__
-        self.assertEqual(rencode.__version__[1:], rencode_orig.__version__[1:], "version number does not match")
+        self.assertEqual(
+            rencode.__version__[1:],
+            rencode_orig.__version__[1:],
+            "version number does not match",
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
